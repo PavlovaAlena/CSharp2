2b075fd1df3efeaf0318f2f6109b708b9b508385
Alena <al_lucky@mail.ru>
2022-07-13 00:38:53 +0400

    Решение второй задачи

---
+++ Seminar2_#13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int d1 = Convert.ToInt32(Console.ReadLine());
+
+if (d1 > 99)
+{
+    int d2 = d1;
+    while (d2 / 10 > 99)
+    {
+        d2 = d2/10;
+    }
+    Console.WriteLine($"Третья цифра числа {d1} это {d2 % 10}");
+}
+else
+{
+    Console.WriteLine($"У числа {d1} третьей цифры нет");
+}
