abed2d8046feb538dc235198c331d3039811f4d7
Alena <al_lucky@mail.ru>
2022-07-13 00:46:26 +0400

    Решение третьей задачи

---
+++ Seminar2_#15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число дня недели: ");
+int dn = Convert.ToInt32(Console.ReadLine());
+
+if (dn >0 && dn < 8)
+{
+    if (dn < 6)
+    {
+        Console.WriteLine($"Числу {dn} соответствует будний день");
+    }
+    else
+    {
+        Console.WriteLine($"Числу {dn} соответствует выходной день");
+    }
+}
+else
+{
+    Console.WriteLine($"Числу {dn} нет соответствия дня недели");
+}
