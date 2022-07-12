98e100ea49f205d3d79c6847ddb2661f61a96e39
Alena <al_lucky@mail.ru>
2022-07-12 23:47:18 +0400

    Решение первой задачи

---
+++ Seminar2_#10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int d1 = Convert.ToInt32(Console.ReadLine());
+
+if (d1 > 99 && d1 < 1000)
+{
+    Console.WriteLine($"Вторая цифра числа {d1} это {d1 / 10 % 10}");
+}
+else
+{
+    Console.WriteLine($"Число {d1} не является 3хзначным");
+}
