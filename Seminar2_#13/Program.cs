4bc63bf518d1aa8371aaa532dda72c0102a2943c
Alena <al_lucky@mail.ru>
2022-07-13 10:04:28 +0400

    Красота в решении

--- Seminar2_#13/Program.cs
+++ Seminar2_#13/Program.cs
@@ -1,16 +1,20 @@
-﻿Console.Write("Введите число: ");
-int d1 = Convert.ToInt32(Console.ReadLine());
+﻿//Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.Write("Программа показывает третью цифру заданного числа");
+Console.WriteLine("");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-if (d1 > 99)
+//if (num > 99 | (num < 0 && -num > 99))
+if (Math.Abs(num) > 99)
 {
-    int d2 = d1;
-    while (d2 / 10 > 99)
+    int numCalc = Math.Abs(num);
+    while (numCalc / 10 > 99)
     {
-        d2 = d2/10;
+        numCalc = numCalc / 10;
     }
-    Console.WriteLine($"Третья цифра числа {d1} это {d2 % 10}");
+    Console.WriteLine($"Третья цифра числа {num} это {numCalc % 10}");
 }
 else
 {
-    Console.WriteLine($"У числа {d1} третьей цифры нет");
+    Console.WriteLine($"У числа {num} третьей цифры нет");
 }
